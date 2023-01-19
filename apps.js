9b3bfb6156c4b63a33c74756c1e8180d8e4ebaa9
ricardo.m21 <ricardoabraham00@gmail.com>
2023-01-19 10:19:06 -0300

    simulando un carrito de compras

--- apps.js
+++ apps.js
@@ -1,115 +1,195 @@
-alert("Bienvenidos A Licoreria El Palmar")
-// validando datos 
-let nombre = prompt("Ingrese su Nombre")
-let apellido = prompt("Ingrese su Apellido")
-let dni = prompt("Ingrese su Dni")
-let edad = prompt("Ingrese su Edad")
-alert("Hola " + nombre)
+alert("Bienvenidos a Licoreria El Palmar")
 
+// constructores y arrays y sus push
+
+class Clientes{
+    constructor(nombre, apellido, edad, dni){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni
+    }
+}
+
+// pido los datos por el promt 
+let nombre = prompt("Ingrese Nombre")
+let apellido = prompt("Ingrese Apellido")
+let edad = prompt("Ingrese su edad ")
+let dni = prompt("Ingrese Dni")
+
+let cliente = new Clientes(nombre, apellido, edad, dni)
+console.log(cliente)
 
 if (edad >= 18) {
     alert ("Que deseas Comprar?")
 } else { edad < 18
     alert("Edad no apta para Comprar")
 }
-// lista de productos 
-let productos = [
-    {nombre: "Cerveza", precio: 2500},
-    {nombre: "Ginebra", precio: 3500},
-    {nombre: "Tequila", precio: 5500},
-    {nombre: "Ron", precio: 8500},
-    {nombre: "Vodka", precio: 9500},
-    {nombre: "Whisky", precio: 10500},
-]
 
-let carrito = []
-// creando el menu 
-function mostrarMenu() { 
-    let menu = "1. Mostrar productos\n2. Comprar producto\n3. Eliminar producto\n4. Ver carrito\n5. Salir"; 
-    let opcion = prompt(menu); 
-    if(!opcion) return mostrarMenu(); 
-    switch(opcion) { 
-    case "1": 
-    mostrarProductos(); 
-    break; 
-    case "2": 
-    comprarProducto(); 
-    break; 
-    case "3": 
-    eliminarProducto(); 
-    break; 
-    case "4": 
-    verCarrito(); 
-    break; 
-    case "5": 
-    alert("¡Gracias por su compra!"); 
-    return; 
-    default: 
-    alert("Opción inválida. Por favor, inténtelo de nuevo."); 
-    } 
-     
-    // Repetir menú 
-    mostrarMenu(); 
-    } 
-     
-    // Función para mostrar los productos disponibles 
-    function mostrarProductos() { 
-    let listaProductos = "Productos disponibles:\n"; 
-    productos.forEach(function(producto) { 
-    listaProductos += producto.nombre + " - $" + producto.precio + "\n"; 
-    }); 
-    alert(listaProductos); 
-    } 
-     
-    // Función para comprar un producto 
-    function comprarProducto() { 
-    let nombreProducto = prompt("Ingrese el nombre del producto que desea comprar:"); 
-    if(!nombreProducto) return mostrarMenu(); 
-    let producto = productos.find(function(producto) { 
-    return producto.nombre.toLowerCase() === nombreProducto.toLowerCase(); 
-    }); 
-    if (producto) { 
-    carrito.push(producto); 
-    alert(producto.nombre + " agregado al carrito."); 
-    } else { 
-    alert("Producto no encontrado."); 
-    } 
-    } 
-     
-    // Función para eliminar un producto del carrito 
-    function eliminarProducto() { 
-    let nombreProducto = prompt("Ingrese el nombre del producto que desea eliminar:"); 
-    if(!nombreProducto) return mostrarMenu(); 
-    let indiceProducto = carrito.findIndex(function(producto) { 
-    return producto.nombre.toLowerCase() === nombreProducto.toLowerCase(); 
-    }); 
-    if (indiceProducto !== -1) { 
-    carrito.splice(indiceProducto, 1); 
-    alert(nombreProducto + " eliminado del carrito."); 
-    } else { 
-    alert("Producto no encontrado en el carrito."); 
-    } 
-    } 
-     
-    // Función para ver los productos en el carrito 
-    function verCarrito() { 
-    if (carrito.length === 0) { 
-    alert("Su carrito está vacío."); 
-    } else { 
-    let listaCarrito = "Productos en su carrito:\n"; 
-    let total = 0; 
-    carrito.forEach(function(producto) { 
-    listaCarrito += producto.nombre + " - $" + producto.precio + "\n"; 
-    total += producto.precio; 
-    }); 
-    alert(listaCarrito + "Total: $" + total); 
-    } 
-    } 
-     
-    // Mostrar menú 
-    mostrarMenu();
+class Licores{
+    constructor(nombre, proveedor, precio){
+        this.nombre = nombre
+        this.proveedor = proveedor
+        this.precio = precio
+    }
+    
+}
 
-alert("Vuelve pronto")
+const licores = []
+
+licores.push(new Licores("Ron", "Crm",1500))
+licores.push(new Licores("Whisky", "Metropolitan",15500))
+licores.push(new Licores("Ginebra","Marfran",11500))
+licores.push(new Licores("Tequila","Nube Azul",12200))
+licores.push(new Licores("Vodka","Alnova",1500))
+
+class Accesorios{
+    constructor(nombre, proveedor, precio){
+    this.nombre = nombre
+    this.proveedor = proveedor
+    this.precio = precio
+    }
+}
+
+const accesorios = []
+
+accesorios.push(new Accesorios("Escarchador", "Soyfer",11500))
+accesorios.push(new Accesorios("Vasos", "Lavanim",12500))
+accesorios.push(new Accesorios("Esterilla", "StarHouse",5500))
+accesorios.push(new Accesorios("Copas", "Lavanim",9500))
+accesorios.push(new Accesorios("Mortero", "Barak",1500))
+
+
+// funciones
+const buscarObjeto = (array, busqueda) => {return array.find(articulo => articulo === busqueda)}
+const sumarCompras = (n1, n2,) => {return resultado = n1 + n2 }
+const sumarIva = (n1) => {return resultado = n1 * 1.21}
+const descuentoEfectivo = (n1 => {return resultado = n1 - (n1 *0.1)})
+
+// nuestra variedad de Licores
+
+alert(" Aca te dejamos lista de nuestros Licores")
+
+// lista de productos
+
+licores.forEach(element => {
+    alert(`Lista de Licores:  ${element.nombre},  Proveedor: ${element.proveedor}, Precio: ${element.precio}` )
+}
+    )
+
+    let elegirLicores = prompt("indique el nombre del licor que desea").toLocaleLowerCase
+    let compraLicores
+    let nombreLicores
+
+    switch (elegirLicores) {
+        case "Ron":
+            let compra = licores[0]
+            compraLicores = compra.precio
+            nombreLicores = compra.nombre
+            break;
+
+            case "Whisky":
+                let compra1 = licores[1]
+                compraLicores = compra1.precio
+                nombreLicores = compra1.nombre
+                break;
+
+            case "Ginebra":
+                let compra2 = licores[2]
+                compraLicores = compra2.precio
+                nombreLicores = compra2.nombre
+            break; 
+
+            case "Tequila":
+                let compra3 = licores[3]
+                compraLicores = compra3.precio
+                nombreLicores = compra3.nombre
+            break;  
+
+         case "Vodka":
+         let compra4 = licores[4]
+         compraLicores = compra4.precio
+         nombreLicores = compra4.nombre
+        break;
+    
+        default:
+            break;
+    }
+
+console.log(compraLicores)
+
+// lista accesorios
+alert("Aca tienes nuestra lista de Accesorios")
+
+accesorios.forEach(element => {
+    alert(`Lista de Accesorios:  ${element.nombre},  Proveedor: ${element.proveedor}, Precio: ${element.precio}` )
+}
+    )
+
+    let elegirAccesorios = prompt("indique el nombre del Accesorio que desea").toLocaleLowerCase
+    let compraAccesorios
+    let nombreAccesorios
+
+    switch (elegirAccesorios) {
+        case "Escarchador":
+            let compra = accesorios[0]
+            compraAccesorios = compra.precio
+            nombreAccesorios = compra.nombre
+            break;
+
+            case "Vasos":
+                let compra1 =  accesorios[1]
+                compraAccesorios = compra1.precio
+                nombreAccesorios = compra1.nombre
+                break;
+
+            case "Esterilla":
+                let compra2 =  accesorios[2]
+                compraAccesorios = compra2.precio
+                nombreAccesorios = compra2.nombre
+            break; 
+
+            case "Copas":
+                let compra3 =  accesorios[3]
+                compraAccesorios = compra3.precio
+                nombreAccesorios = compra3.nombre
+            break;  
+
+         case "Mortero":
+         let compra4 =  accesorios[4]
+         compraAccesorios = compra4.precio
+         nombreAccesorios = compra4.nombre
+        break;
+    
+        default:
+            break;
+    }
+
+console.log(compraAccesorios)
+
+// la compra
+
+let compraFinal = sumarCompras(compraLicores, compraAccesorios)
+console.log(compraFinal)
+alert(`Compraste ${nombreLicores}, y un ${nombreAccesorios}. El Total es $ ${compraFinal}`)
+
+let compraConIva = sumarIva(compraFinal)
+console.log(compraConIva)
+
+alert(`Tu Compra con IVA incluido total $ ${compraConIva}.`)
+
+// pago en efectivo
+alert("Tienes 10% pagando en efectivo")
+let metodoDePago = prompt("Abonas en efectivo?")
+let precioFinal
+  
+if (metodoDePago == "efectivo") {
+    precioFinal = descuentoEfectivo(compraConIva)
+    alert(`El precio final es $${precioFinal}`)
+} else (metodoDePago != "efectivo" )
+    alert("No tienes descuento")
+
+    alert("Gracias por tu compra")
 
 
 
